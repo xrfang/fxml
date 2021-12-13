012c3ee5894e65b8db8b550813e0b00b454922be
xrfang <xrfang@gmail.com>
2021-12-13 15:29:30 +0800

    added walk example

--- example_test.go
+++ example_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/xrfang/fxml"
 )
@@ -128,4 +129,40 @@ func ExampleXMLTree_Traverse() {
 	//kml
 	//kml/Document
 	//kml/Document/name
+}
+
+// walk part of a XMLTree and modify node text
+func ExampleXMLTree_Walk() {
+	bs := bytes.NewBufferString(`<?xml version="1.0" encoding="UTF-8"?>
+	<style>This is root
+	  <LineStyle>
+        <color>red</color>
+        <width>1</width>
+	  </LineStyle>
+	  <PolyStyle>
+		<color>green</color>
+		<width>2</width>
+	  </PolyStyle>
+	</style>`)
+	xt, _ := fxml.Parse(bs)
+	xt.Walk(func(ni fxml.XNodInfo, x *fxml.XMLTree) fxml.XWalkResult {
+		fmt.Printf("%s|%+v\n", strings.Join(ni.Path, "/"), ni)
+		x.Text = strings.ToUpper(x.Text)
+		if ni.Path[len(ni.Path)-1] == "color" {
+			return fxml.WRSkip
+		}
+		if ni.Path[len(ni.Path)-1] == "PolyStyle" {
+			return fxml.WRTerm
+		}
+		return fxml.WRCont
+	})
+	je := json.NewEncoder(os.Stdout)
+	je.Encode(xt)
+	//Output:
+	//style|{Path:[style] Index:0 RIndex:0}
+	//style/|{Path:[style ] Index:0 RIndex:-3}
+	//style/LineStyle|{Path:[style LineStyle] Index:1 RIndex:-2}
+	//style/LineStyle/color|{Path:[style LineStyle color] Index:0 RIndex:-2}
+	//style/PolyStyle|{Path:[style PolyStyle] Index:2 RIndex:-1}
+	//{"Name":{"Space":"","Local":"style"},"Children":[{"Name":{"Space":"","Local":""},"Text":"THIS IS ROOT"},{"Name":{"Space":"","Local":"LineStyle"},"Children":[{"Name":{"Space":"","Local":"color"},"Text":"RED"},{"Name":{"Space":"","Local":"width"},"Text":"1"}]},{"Name":{"Space":"","Local":"PolyStyle"},"Children":[{"Name":{"Space":"","Local":"color"},"Text":"green"},{"Name":{"Space":"","Local":"width"},"Text":"2"}]}]}
 }
