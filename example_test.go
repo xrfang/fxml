74d9f39f2aaa16919e44669a649a106a3647f2c3
xrfang <xrfang@gmail.com>
2022-08-24 14:33:46 +0800

    fixed typo in document

--- example_test.go
+++ example_test.go
@@ -105,7 +105,7 @@ func ExampleXMLTree_Encode() {
 	//Output: <?xml version="1.0" encoding="UTF-8"?><kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><Document id="example"><name>Map of the region</name></Document></kml>
 }
 
-// travers a XMLTree
+// traverse a XMLTree
 func ExampleXMLTree_Traverse() {
 	b := bytes.NewBufferString(`<?xml version="1.0" encoding="UTF-8"?>
 	<kml xmlns="http://www.opengis.net/kml/2.2"
